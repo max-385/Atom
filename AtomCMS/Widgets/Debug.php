ff101d54eb5875b6809595176fc35ce0d88c36e6
Max <maximsa@inbox.lv>
2020-03-12 01:20:31 +0200

    small cosmetic changes

--- AtomCMS/Widgets/Debug.php
+++ AtomCMS/Widgets/Debug.php
@@ -4,9 +4,9 @@
 	<?php $all_vars = get_defined_vars(); ?>
 	
 	<pre>
-<H1>All varriables:</H1>
+<H1 style="background-color: cadetblue ">All varriables:</H1>
 		<?php print_r($all_vars); ?> <!-- Print array of all defined varriables -->
-<H1>Page array:</H1>
+<H1 style="background-color: cadetblue">Page array:</H1>
 		<?php print_r($page); ?> <!-- Print array info (if debug is clicked)-->
 	</pre>
 	
