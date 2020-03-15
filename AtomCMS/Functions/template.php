cda9802d79cff2d15464eff7012d3e443cc2d536
Max <maximsa@inbox.lv>
2020-03-15 02:25:42 +0200

    Spell mistake

--- AtomCMS/Functions/template.php
+++ AtomCMS/Functions/template.php
@@ -6,7 +6,7 @@ function nav_main($dbc, $pageid) {
 	$result = mysqli_query($dbc, $query);
 	
 	while($nav = mysqli_fetch_assoc($result)) {			
-		// Anotrer way		echo '<li class="nav-item"><a class="nav-link" href="?page='.$nav['id'].'">'.$nav['title'].'</a></li>';
+		// Another way		echo '<li class="nav-item"><a class="nav-link" href="?page='.$nav['id'].'">'.$nav['title'].'</a></li>';
 		?>
 				<li class="nav-item<?php if ($pageid == $nav['id']) 
 					{echo ' active';}?>"><a class="nav-link" href="?page=<?php echo $nav['id'];?>"><?php echo $nav['label'];?></a></li>
