02188667b27fa85e47814109f8c7e21600f9a300
Max <maximsa@inbox.lv>
2020-03-16 22:58:37 +0200

    spell

--- AtomCMS/Widgets/Debug.php
+++ AtomCMS/Widgets/Debug.php
@@ -4,8 +4,8 @@
 	<?php $all_vars = get_defined_vars(); ?>
 	
 	<pre>
-<H1 style="background-color: cadetblue ">All varriables:</H1>
-		<?php print_r($all_vars); ?> <!-- Print array of all defined varriables -->
+<H1 style="background-color: cadetblue ">All variables:</H1>
+		<?php print_r($all_vars); ?> <!-- Print array of all defined variables -->
 <H1 style="background-color: cadetblue">Page array:</H1>
 		<?php print_r($page); ?> <!-- Print array info (if debug is clicked)-->
 	</pre>
