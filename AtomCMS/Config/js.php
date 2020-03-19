16044dc58f4454f4f1e592a738e0bbc520256c94
Max <maximsa@inbox.lv>
2020-03-19 20:42:51 +0200

    bootstrap version update

--- AtomCMS/Config/js.php
+++ AtomCMS/Config/js.php
@@ -17,10 +17,9 @@
   crossorigin="anonymous"></script>
 
 <!-- Bootstrap JavaScript jQuery -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
-integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
 crossorigin="anonymous"></script>
-
 
 <script>
 	
