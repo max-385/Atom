8ffd177e0af1b16fee5c4f24b77f1f68a4cc7283
Max <maximsa@inbox.lv>
2020-04-07 23:06:16 +0300

    Row margin

--- admin/Config/css.php
+++ admin/Config/css.php
@@ -2,51 +2,54 @@
 //css file
 
 
-
-
 ?>
 
 <!-- Bootstrap css-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 <!-- jQuery CSS -->
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css"
 
 <!-- Font Awesome -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
-integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
 <style>
-	html, 
-	body {
-		height:100%;
-	}
-	
-/*	#btn-debug {
-		position: absolute;
-        right:5px;
-	}
-*/
-	#console-debug {
-		position: absolute;
-		top: 60px;
-		background: #dddddd;
-		width: 35%;
-		height: 500px;
-		box-shadow: 2px 2px 5px #CCCCCC;
-	}
-	
-	#console-debug pre {
+    html,
+    body {
+        height: 100%;
+    }
+
+    /*	#btn-debug {
+            position: absolute;
+            right:5px;
+        }
+    */
+    #console-debug {
         position: absolute;
-        left:10px;
-        right:10px;
-		height: 500px;
-		white-space: pre-wrap;       /* css-3 */
-	}
+        top: 60px;
+        background: #dddddd;
+        width: 35%;
+        height: 500px;
+        box-shadow: 2px 2px 5px #CCCCCC;
+    }
+
+    #console-debug pre {
+        position: absolute;
+        left: 10px;
+        right: 10px;
+        height: 500px;
+        white-space: pre-wrap; /* css-3 */
+    }
 
     #console-debug pre H1 {
-        text-align:center;
+        text-align: center;
         background-color: cadetblue;
     }
+
+    .row {
+        margin: auto;
+    }
+
 </style>
